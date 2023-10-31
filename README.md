8e7ec08619555ad6b61e4d23cd9f7bac80af9c3f
forrestdavis <forrestd@mit.edu>
2023-10-31 19:56:44 -0400

    Added part 3

--- README.md
+++ README.md
@@ -101,6 +101,64 @@ code**. You should review the feedforward neural network lecture notes
 ## Part 3 - Evaluating 
 
 In this portion you will evaluate a pre-trained model for linguistic knowledge.
-More details will be added later. 
+In particular, you will evaluate a tiny version of GPT-2 called
+[distilgpt2](https://huggingface.co/distilgpt2). You are tasked with
+benchmarking distilgpt2's linguistic knowledge. Helper code that loads the
+pre-trained model and that returns by-word surprisals/probabilities is included
+in ling.py. In order to run this code, you have to install HuggingFace's
+transformers library. Installation instructions can be found
+[here](https://github.com/huggingface/transformers#installation).
+
+How should you evaluate your model's linguistic knowledge? This problem is
+intentionally open ended to give you practice creatively approaching problems.
+As such there is no right answer, instead you should justify why your approach
+makes sense and how you came to your conclusions. Please include this in a
+multi-line comment at the top of your code or in a pdf, whichever you prefer. 
+
+Still unsure how you might approach this. Here's one way we've covered in Week
+11's lectures. Suppose you were interested in subject-verb agreement and whether
+this model had learned the relevant linguistic behavior. You could construct
+minimal pairs like the following. 
+
+    a) The cat is so happy
+    b) The cat are so happy
+
+We could get the by\_word probabilities from distilgpt2 using the included code: 
+
+    >>> from ling import *
+    >>> model, tokenizer = load_pretrained_model()
+    >>> sent_a = 'The cat is happy'
+    >>> sent_b = 'The cat are happy'
+    >>> a_probs = get_aligned_words_measures(sent_a, 'prob', model, tokenizer)
+    >>> b_probs = get_aligned_words_measures(sent_b, 'prob', model, tokenizer)
+    >>> a_probs
+    [('The', 0), ('cat', 1.437380433344515e-06), ('is', 0.05121663585305214), ('happy', 0.0012213564477860928)]
+    >>> b_probs
+    [('The', 0), ('cat', 1.437380433344515e-06), ('are', 0.0011601115111261606), ('happy', 0.0011389038991183043)]
+
+We see that both the verb and the whole sentence is more likely for a than for
+b. If this happens with more sentences, we might conclude that distilgpt2 has
+captured a basic aspect of subject-verb agreement. We can feel more confident if
+we try other constructions that get at the same process. For example, 
+
+    c) The cat near the books is so happy
+    d) The cat near the books are so happy
+
+Here it is slightly harder to keep track of the subject-verb dependency. We can
+run the same code: 
+
+    >>> from ling import *
+    >>> model, tokenizer = load_pretrained_model()
+    >>> sent_a = 'The cat near the books is happy'
+    >>> sent_b = 'The cat near the books are happy'
+    >>> a_probs = get_aligned_words_measures(sent_a, 'prob', model, tokenizer)
+    >>> b_probs = get_aligned_words_measures(sent_b, 'prob', model, tokenizer)
+    >>> a_probs
+    [('The', 0), ('cat', 1.437380433344515e-06), ('near', 6.501279858639464e-05), ('the', 0.34228000044822693), ('books', 3.5550870961742476e-05), ('is', 0.059263549745082855), ('happy', 0.0005464301793836057)]
+    >>> b_probs
+    [('The', 0), ('cat', 1.437380433344515e-06), ('near', 6.501279858639464e-05), ('the', 0.34228000044822693), ('books', 3.5550870961742476e-05), ('are', 0.004924507811665535), ('happy', 0.0004336706770118326)]
+
+We see again that the model is correct. You might try this basic approach with a
+different linguistic process.
 
 Good luck :) !
--- ling.py
+++ ling.py
@@ -1,7 +1,98 @@
-import torch
 import sys
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-from src.Tokenizer import Tokenizer
-from model import RNNModel
+def load_pretrained_model(name:str='distilgpt2') -> tuple[GPT2LMHeadModel,
+                                                          GPT2Tokenizer]:
+    """Loads a pretrained auto-regressive language model from 
+        HuggingFace
+
+    Args:
+        name (str): Name of model
+    Returns:
+        model (GPT2LMHeadModel): Pretrained model
+        tokenizer (GPT2Tokenizer): Tokenizer
+    """
+    tokenizer = GPT2Tokenizer.from_pretrained(name)
+    tokenizer.pad_token = tokenizer.eos_token
+    model = GPT2LMHeadModel.from_pretrained(name)
+    model.eval()
+    return model, tokenizer
+
+@torch.no_grad()
+def get_aligned_words_measures(text: str, 
+                               measure: str,
+                               model: GPT2LMHeadModel, 
+                               tokenizer: GPT2Tokenizer) -> list[str]:
+    """ Returns words and their measure (prob|surp)
+    Args:
+        text (list[str]): list of sentences
+        measure (str): Measure, either probability or surprisal
+                        (options: prob|surp)
+        model (GPT2LMHeadModel): Pretrained model
+        tokenizer (GPT2Tokenizer): Tokenizer
+    Returns:
+        list[str]: List of words with their measures
+
+    For example, 
+    >>> model, tokenizer = load_pretrained_model()
+    >>> get_aligned_words_measures('the student is happy', 
+    ...        'surp', model, tokenizer)
+    [('the', 0), ('student', 17.38616943359375), ('is', 6.385905742645264),
+     ('happy', 9.564245223999023)]
+    >>> get_aligned_words_measures('the cat is fluffy', 
+    ...        'prob', model, tokenizer) 
+    [('the', 0), ('cat', 2.5601848392398097e-06), ('is', 0.025296149775385857),
+     ('fluffy', 0.00020585735910572112)]
+    >>> get_aligned_words_measures('the cat are fluffy', 
+    ...        'prob', model, tokenizer)
+    [('the', 0), ('cat', 2.5601848392398097e-06), ('are', 0.0010310395155102015),
+     ('fluffy', 0.00021902224398218095)]
+    """
+    if measure not in {'prob', 'surp'}:
+        sys.stderr.write(f"{measure} not recognized\n")
+        sys.exit(1)
+
+    data = []
+
+    ids = tokenizer(text, return_tensors='pt')
+    input_ids = ids.input_ids.flatten().data
+    target_ids = ids.input_ids[:,1:]
+
+    # get output
+    logits = model(**ids).logits
+    output = torch.nn.functional.log_softmax(logits, dim=-1)
+    if measure == 'surp':
+        output = -(output/torch.log(torch.tensor(2.0)))
+    else:
+        output = torch.exp(output)
+
+    # get by token measures 
+    target_measures = output[:,:-1, :]
+    # use gather to get the output for each target item in the batch
+    target_measures = target_measures.gather(-1,
+                             target_ids.unsqueeze(2)).flatten().tolist()
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    words = text.split(' ')
+
+    # A lil loop to force align words 
+    current_word = words.pop(0)
+    current_token = tokens.pop(0)
+    measure = 0
+    while len(data) != len(text.split(' ')):
+        if current_word == current_token:
+            data.append((current_word, measure))
+            measure = 0
+            if words:
+                current_word = words.pop(0)
+                current_token = tokens.pop(0).replace('Ġ', '')
+                measure += target_measures.pop(0)
+        else:
+            measure += target_measures.pop(0)
+            current_token += tokens.pop(0).replace('Ġ', '')
+
+    return data
 
 # TODO: Your code goes here
+
+
